9c5517ff6cab59b92c40b70b23a02f4dec138a1b
JOHN-KUN1 <ritjijohn123@gmail.com>
2024-05-26 20:59:30 +0100

    changes

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-    const responseText = "THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.<br>NAME - RITJI JOHN GOFWEN. MAT NO - VUG / CSC / 21 / 5440";
-    res.send(responseText);
-}); 
+// app.get('/', (req, res) => {
+//     const responseText = "THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.<br>NAME - RITJI JOHN GOFWEN. MAT NO - VUG / CSC / 21 / 5440";
+//     res.send(responseText);
+// }); 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
