2dedce59fae0dc79939c67e09c37b2cd11cf7be7
JOHN-KUN1 <ritjijohn123@gmail.com>
2024-05-26 12:45:59 +0100

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
 
 app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS ; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME - RITJI JOHN GOFWEN. MAT NO - VUG/CSC/21/5440'));
 
