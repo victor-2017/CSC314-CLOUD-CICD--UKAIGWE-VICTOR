d6f92341e0aea5c330e89937694c18f3b2bcbae5
ukaigwe-victor <ukaigwevictor62gmail.com>
2024-05-27 18:22:30 +0100

    change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 8080;
 app.use(express.static("public"))
-// app.get('/', (req, res) => res.send("THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME -- RITJI JOHN GOFWEN.MAT NO -- VUG / CSC / 21 / 5440"));
+app.get('/', (req, res) => res.send("THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME -- ukaigwe victor.MAT NO -- VUG / CSC / 21 / 5173"));
 
 
 
